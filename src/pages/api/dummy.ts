4a724842ed165aae1ae4b381da42363521f3b584
hanyugeon <hanyugeon@naver.com>
2023-02-15 02:22:01 +0900

    Chore: 더미 데이터 정보 수정

--- src/pages/api/dummy.ts
+++ src/pages/api/dummy.ts
@@ -1,18 +1,18 @@
-type COFFEE_TYPES = { title: string; isHottest: boolean }[]
+type COFFEE_TYPES = { title: string }[]
 
 const DUMMY_DATA: COFFEE_TYPES = [
-  { title: '에스프레소', isHottest: true },
-  { title: '아메리카노', isHottest: true },
-  { title: '카페라떼', isHottest: true },
-  { title: '카푸치노', isHottest: false },
-  { title: '카페모카', isHottest: true },
-  { title: '카라멜마키아토', isHottest: false },
-  { title: '얼그레이', isHottest: true },
-  { title: '페퍼민트', isHottest: true },
-  { title: '루이보스', isHottest: false },
-  { title: '레몬에이드', isHottest: true },
-  { title: '자몽에이드', isHottest: true },
-  { title: '아이스티', isHottest: false },
+  { title: '에스프레소' },
+  { title: '아메리카노' },
+  { title: '카페라떼' },
+  { title: '카푸치노' },
+  { title: '카페모카' },
+  { title: '카라멜마키아토' },
+  { title: '얼그레이' },
+  { title: '페퍼민트' },
+  { title: '루이보스' },
+  { title: '레몬에이드' },
+  { title: '자몽에이드' },
+  { title: '아이스티' },
 ]
 
 export { DUMMY_DATA }
