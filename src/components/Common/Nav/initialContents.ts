85c27f325eb315cdd608cd720e96902ced659e0b
hanyugeon <hanyugeon@naver.com>
2023-03-27 15:57:21 +0900

    Chore: NavItem initialContents 수정

--- src/components/Common/Nav/initialContents.ts
+++ src/components/Common/Nav/initialContents.ts
@@ -1,15 +1,14 @@
 import { icGithub } from '@/assets/icons'
 
 const NAV_ROUTE_CONTENTS = [
-  { href: '/', variant: 'menu', title: '커피 목록' },
-  { href: '/tech', variant: 'menu', title: '기술 스택' },
-  { href: '/about', variant: 'menu', title: '만든 계기' },
+  { href: '/', title: '커피 목록' },
+  { href: '/tech', title: '기술 스택' },
+  { href: '/about', title: '만든 계기' },
 ]
 
 const NAV_LINK_CONTENTS = [
   {
     href: 'https:/github.com/hanyugeon',
-    variant: 'link',
     title: 'github',
     icon: icGithub,
   },
