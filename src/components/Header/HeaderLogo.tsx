cec7900910f9f3f8ff8eb9a377ea127a9cc3ce58
hanyugeon <hanyugeon@naver.com>
2023-03-27 15:56:55 +0900

    Feat: 로고 클릭시 라우팅 이벤트 추가

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -1,11 +1,14 @@
 import { icLogo } from '@/assets/icons'
 import Image from 'next/image'
+import Link from 'next/link'
 import { StyledHeaderLogo } from './styled'
 
 const HeaderLogo = () => {
   return (
     <StyledHeaderLogo>
-      <Image width={244} height={90} src={icLogo} alt="" priority={true} />
+      <Link href="/">
+        <Image width={244} height={90} src={icLogo} alt="" priority={true} />
+      </Link>
     </StyledHeaderLogo>
   )
 }
