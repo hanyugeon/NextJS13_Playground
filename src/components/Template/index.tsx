8323ef6e186affc0ad87f612f307130552600e1b
hanyugeon <hanyugeon@naver.com>
2023-03-27 17:28:56 +0900

    Feat: SideBar 컴포넌트 useKeyPress 적용

--- src/components/Template/index.tsx
+++ src/components/Template/index.tsx
@@ -2,7 +2,9 @@
 
 import Header from '@/components/Header'
 import SideBar from '@/components/SideBar'
+import useKeyPress from '@/hooks/useKeyPress'
 import useToggle from '@/hooks/useToggle'
+import { useEffect } from 'react'
 import * as S from './styled'
 
 type ChildrenType = {
@@ -11,6 +13,13 @@ type ChildrenType = {
 
 const Template = ({ children }: ChildrenType) => {
   const [isSideBarOpen, toggleSideBar] = useToggle()
+  const escKeyPressed = useKeyPress('Escape')
+
+  useEffect(() => {
+    if (isSideBarOpen && escKeyPressed) {
+      return toggleSideBar()
+    }
+  }, [isSideBarOpen, toggleSideBar, escKeyPressed])
 
   return (
     <>
