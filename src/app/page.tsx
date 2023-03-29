8f21f924fbfa43e11e89c5f7447f68675e7240a6
hanyugeon <hanyugeon@naver.com>
2023-03-30 04:08:55 +0900

    Feat: Server Component에서 api 연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
+import { getCoffeeData } from '@/pages/api/coffee'
 import CoffeeItem from '@/ui/CoffeeItem'
 import CoffeeList from '@/ui/CoffeeList'
-import { DUMMY_DATA } from '@/pages/api/dummy'
 
-const Home = () => {
+type CoffeeDataType = {
+  title: string
+  description: string
+  ingredients: string[]
+  image: string
+  id: number
+}
+
+async function Home() {
+  const coffeeData: CoffeeDataType[] = await getCoffeeData()
+
   return (
     <CoffeeList>
-      {DUMMY_DATA.map((data) => (
-        <CoffeeItem key={data.title} coffeeTitle={data.title} />
+      {coffeeData.map((coffee) => (
+        <CoffeeItem
+          key={coffee.id}
+          coffeeTitle={coffee.title}
+          coffeeImage={coffee.image}
+        />
       ))}
     </CoffeeList>
   )
