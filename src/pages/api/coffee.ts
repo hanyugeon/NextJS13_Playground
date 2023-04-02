c29580d793d4e2f582248fbe4a23bdca6a036c0a
hanyugeon <hanyugeon@naver.com>
2023-04-02 19:14:46 +0900

    Chore: 기존 Fetch 메서드 삭제

--- src/pages/api/coffee.ts
+++
@@ -1,9 +0,0 @@
-export async function getCoffeeData() {
-  const response = await fetch('https://api.sampleapis.com/coffee/hot')
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return response.json()
-}
