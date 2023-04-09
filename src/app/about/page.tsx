06431812c33f1e4a31d4e0ff13ae856065baaba0
hanyugeon <hanyugeon@naver.com>
2023-04-10 02:46:24 +0900

    Chore: about 페이지 임시로 보류

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,26 +2,27 @@ import type { AboutDataType } from '@/types/about'
 import CoffeeItem from '@/ui/CoffeeItem'
 import CoffeeList from '@/ui/CoffeeList'
 
-async function getStaticAboutData() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/about`)
+// async function getStaticAboutData() {
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/about`)
 
-  if (!response.ok) throw new Error('Failed to fetch data')
+//   if (!response.ok) throw new Error('Failed to fetch data')
 
-  return await response.json()
-}
+//   return await response.json()
+// }
 
 export default async function Tech() {
-  const aboutData: AboutDataType[] = await getStaticAboutData()
+  // const aboutData: AboutDataType[] = await getStaticAboutData()
 
   return (
-    <CoffeeList>
-      {aboutData.map((about, idx) => (
-        <CoffeeItem
-          key={idx}
-          coffeeTitle={about.title}
-          coffeeImage={about.image}
-        />
-      ))}
-    </CoffeeList>
+    <></>
+    // <CoffeeList>
+    //   {aboutData.map((about, idx) => (
+    //     <CoffeeItem
+    //       key={idx}
+    //       coffeeTitle={about.title}
+    //       coffeeImage={about.image}
+    //     />
+    //   ))}
+    // </CoffeeList>
   )
 }
