fb246d8a227609659c6f7905986e5888de2a6912
hanyugeon <hanyugeon@naver.com>
2023-04-09 22:43:24 +0900

    Feat: API 호출함수 작성 및 SSG 구현

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,27 @@
-export default function About() {
-  return <></>
+import type { AboutDataType } from '@/types/about'
+import CoffeeItem from '@/ui/CoffeeItem'
+import CoffeeList from '@/ui/CoffeeList'
+
+async function getStaticAboutData() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/about`)
+
+  if (!response.ok) throw new Error('Failed to fetch data')
+
+  return await response.json()
+}
+
+export default async function Tech() {
+  const aboutData: AboutDataType[] = await getStaticAboutData()
+
+  return (
+    <CoffeeList>
+      {aboutData.map((about, idx) => (
+        <CoffeeItem
+          key={idx}
+          coffeeTitle={about.title}
+          coffeeImage={about.image}
+        />
+      ))}
+    </CoffeeList>
+  )
 }
