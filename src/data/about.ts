78e58e8148277cb24d079215f226e1849716fefc
hanyugeon <hanyugeon@naver.com>
2023-04-09 22:42:31 +0900

    Feat: api data 생성

---
+++ src/data/about.ts
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    title: 'Next와 친해지고 싶었어요',
+    image: '',
+    id: 0,
+  },
+  {
+    title: 'app 디렉토리가 궁금했어요',
+    image: '',
+    id: 1,
+  },
+  {
+    title: 'data-fetch lib도 다루려했어요',
+    image: '',
+    id: 2,
+  },
+  {
+    title: 'app 디렉토리는 fetch()를 권장해요',
+    image: '',
+    id: 3,
+  },
+  {
+    title: '렌더링 방식을 공부하게되었어요',
+    image: '',
+    id: 4,
+  },
+  {
+    title: 'SSR과 SSG를 구현했어요.',
+    image: '',
+    id: 5,
+  },
+  {
+    title: '웹 페이지 성능개선도 해보았어요.',
+    image: '',
+    id: 6,
+  },
+]
