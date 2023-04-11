9ba9989af1e546f19b2f1a8560e273cba55b9ac9
hanyugeon <hanyugeon@naver.com>
2023-04-12 01:38:54 +0900

    Feat: api 매핑 데이터 작성

---
+++ src/utils/mappingApiData.ts
@@ -0,0 +1,15 @@
+import type { NotionDataType, NotionRowType } from '@/types/api'
+
+export const mappingApiData = (data: NotionRowType[]) => {
+  const response: NotionDataType[] = []
+
+  data.map((item, idx) => {
+    response.push({
+      id: idx,
+      title: `${item.properties.title.rich_text[0].plain_text}`,
+      image: '',
+    })
+  })
+
+  return response
+}
