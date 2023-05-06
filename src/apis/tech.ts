a035be33321d7f5ebe19b97744e27ed7730fbad1
hanyugeon <hanyugeon@naver.com>
2023-05-06 17:15:57 +0900

    Refactor: api함수 분리 및 ISR으로 변경

---
+++ src/apis/tech.ts
@@ -0,0 +1,30 @@
+import type { NotionRowType } from '@/types/notionApi'
+import { mappingApiData } from '@/utils/mappingApiData'
+
+const SECRET_KEY = process.env.NOTION_SECRET
+const DATABASE_ID = process.env.NOTION_TECH_DATABASE_ID
+
+export async function getStaticTechData() {
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${SECRET_KEY}`,
+    },
+    body: JSON.stringify({ page_size: 100 }),
+    next: { revalidate: 10 },
+  }
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options,
+  )
+
+  const database = await res.json()
+
+  const data: NotionRowType[] = database.results
+
+  return mappingApiData(data)
+}
