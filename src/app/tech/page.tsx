1554cf2fa64ab9d4a273e0c32548bdb38014edd4
hanyugeon <hanyugeon@naver.com>
2023-05-06 17:15:43 +0900

    Refactor: api호출 함수 분리

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -1,34 +1,7 @@
-import type { NotionDataType, NotionRowType } from '@/types/notionApi'
+import { getStaticTechData } from '@/apis/tech'
+import type { NotionDataType } from '@/types/notionApi'
 import CoffeeItem from '@/ui/CoffeeItem'
 import CoffeeList from '@/ui/CoffeeList'
-import { mappingApiData } from '@/utils/mappingApiData'
-
-const SECRET_KEY = process.env.NOTION_SECRET
-const DATABASE_ID = process.env.NOTION_TECH_DATABASE_ID
-
-async function getStaticTechData() {
-  const options = {
-    method: 'POST',
-    headers: {
-      accept: 'application/json',
-      'Notion-Version': '2022-06-28',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${SECRET_KEY}`,
-    },
-    body: JSON.stringify({ page_size: 100 }),
-  }
-
-  const res = await fetch(
-    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
-    options,
-  )
-
-  const database = await res.json()
-
-  const data: NotionRowType[] = database.results
-
-  return mappingApiData(data)
-}
 
 export default async function Tech() {
   const techData: NotionDataType[] = await getStaticTechData()
