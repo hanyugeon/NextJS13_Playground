48e04f85e9123d76fa5a8a53e923b75850761b23
hanyugeon <hanyugeon@naver.com>
2023-08-12 21:52:31 +0900

    Chore: 서버 에러로 API 불러오지 못하는 부분 주석 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import CoffeeItem from '@/ui/CoffeeItem'
 import CoffeeList from '@/ui/CoffeeList'
 
 export default async function Home() {
-  const coffeeData: CoffeeDataType[] = await getCoffeeData()
+  // const coffeeData: CoffeeDataType[] = await getCoffeeData()
 
   return (
     <CoffeeList>
-      {coffeeData.map((coffee) => (
+      {/* {coffeeData.map((coffee) => (
         <CoffeeItem
           key={coffee.id}
           coffeeTitle={coffee.title}
           coffeeImage={coffee.image}
         />
-      ))}
+      ))} */}
     </CoffeeList>
   )
 }
